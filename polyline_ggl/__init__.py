5b069545482a415c7fa870982096efbf2554c114
mthh <matthieu.viry@cnrs.fr>
2019-07-30 13:17:04 +0200

    Change extensions path and import

--- polyline_ggl/__init__.py
+++ polyline_ggl/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from .decode import decode_polyline
-from .encode import encode_polyline
+from . import decode
+from . import encode
+
+decode_polyline = decode.decode_polyline
+encode_polyline = encode.encode_polyline
 
 __all__ = ["decode_polyline", "encode_polyline"]
--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@
 from distutils.core import setup
 from setuptools import find_packages
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
 
-exts = [Extension("polyline_ggl/decode",
-            ["polyline_ggl/decode.pyx"], ["polyline_ggl"]),
-        Extension("polyline_ggl/encode",
-            ["polyline_ggl/encode.pyx"], ["polyline_ggl"])]
+exts = [Extension("polyline_ggl.decode",
+            ["polyline_ggl/decode.pyx"], ["."]),
+        Extension("polyline_ggl.encode",
+            ["polyline_ggl/encode.pyx"], ["."])]
 setup(
     name="polyline_ggl",
     version="0.0.1",
     license="BSD",
     install_requires = ['cython'],
-    ext_modules=cythonize(exts),
-    cmdclass = {'build_ext': build_ext},
+    ext_modules=exts,
     packages=find_packages(),
     test_suite = 'tests',
 )
